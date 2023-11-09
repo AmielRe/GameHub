85b539d1ac5a2156862f367f243d0e0880c7e774
AmielRe <amieli1999@gmail.com>
2023-11-09 13:41:57 +0200

    Create custom attribute for message classes.

---
+++ Common/Attributes/MessageAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+    sealed class MessageAttribute : Attribute
+    {
+        public string MessageType { get; }
+
+        public MessageAttribute(string messageType)
+        {
+            MessageType = messageType;
+        }
+    }
+}
