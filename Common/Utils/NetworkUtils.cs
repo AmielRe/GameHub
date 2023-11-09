be74ca3c50f7f648f134c26064a427c5325380d0
AmielRe <amieli1999@gmail.com>
2023-11-09 13:25:19 +0200

    Create NetworkUtils.
    This will be used to get the Ipv4 address of the client as the UUID sent to the server.

---
+++ Common/Utils/NetworkUtils.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Utils
+{
+    public static class NetworkUtils
+    {
+        public static string GetIPv4Address()
+        {
+            string ipv4Address = null;
+            NetworkInterface[] networkInterfaces = NetworkInterface.GetAllNetworkInterfaces();
+
+            foreach (NetworkInterface networkInterface in networkInterfaces)
+            {
+                if (networkInterface.OperationalStatus == OperationalStatus.Up &&
+                    (networkInterface.NetworkInterfaceType == NetworkInterfaceType.Ethernet ||
+                     networkInterface.NetworkInterfaceType == NetworkInterfaceType.Wireless80211))
+                {
+                    IPInterfaceProperties ipProperties = networkInterface.GetIPProperties();
+
+                    foreach (UnicastIPAddressInformation ipInfo in ipProperties.UnicastAddresses)
+                    {
+                        if (ipInfo.Address.AddressFamily == AddressFamily.InterNetwork)
+                        {
+                            ipv4Address = ipInfo.Address.ToString();
+                            break;
+                        }
+                    }
+
+                    if (ipv4Address != null)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            return ipv4Address;
+        }
+
+    }
+}
